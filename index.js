1f875ec6da302750e179f10ddf6e9e9615fe73f3
Nicholas Powers <njpowers7915@gmail.com>
2018-04-02 18:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string === ""  {
+  } else if (string === "I love you, Grandma.")  {
+    return "I love you, too."
+  } else {
     return "YES INDEED!"
   }
 }
